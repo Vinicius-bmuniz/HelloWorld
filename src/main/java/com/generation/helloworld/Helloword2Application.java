1de978323c856adf165d343abdb64e8483606e97
Vinicius Barbosa <vinicius.barbos27@gmail.com>
2022-05-26 16:03:56 -0300

    Exercicio Spring Boot

---
+++ src/main/java/com/generation/helloworld/Helloword2Application.java
@@ -0,0 +1,13 @@
+package com.generation.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloword2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helloword2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //essa anotação é para indicar que essa classe é uma classe controladora
+@RequestMapping ("/hello") //Não deixar espaços em brancos abaixo das anotações
+public class HelloController {
+	
+	@GetMapping
+	public String HellowWorld() {
+		return "<title> Projeto Generation </title>" + "<font size = 7> Hello Generation";
+	}
+	
+	@GetMapping("/bsm")
+	public String Bsm() {
+		return "<title> Projeto Generation </title>" + "<font size = 7> <b>Habilidades" + "<font size = 3><br> \t • Trabalho em equipe " + "<font size = 3><br> \t • Orientação ao detalhe" + "<font size = 3><br> \t • Proatividade" + "<font size = 3><br> \t • Comunicação" + "<br><br><font size = 7> <b>Mentalidades" + "<font size = 3><br> \t • Orientação ao Futuro" + "<font size = 3><br> \t • Responsabilidade Pessoal" + "<font size = 3><br> \t • Mentalidade de Crescimento" + "<font size = 3><br> \t • Persistência";
+	}
+	
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<title> Projeto Generation </title>" + "<font size = 7> As metas de aprendizado para essa semana são as seguintes:" + "<font size = 4><br>- Aprender mais sobre Spring Boot;" + "<br>- Terminar o módulo de MySQL;" + "<br>- Completar o escopo do projeto integrador.";
+	}
+	
+	
+}
